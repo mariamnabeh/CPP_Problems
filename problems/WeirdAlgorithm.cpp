a9dace5a5d6f949cff9bd5907a5ae5fb70523f24
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-04-02 01:39:03 +0200

    weird Algorthim problem

---
+++ problems/WeirdAlgorithm.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+#define el "\n"
+typedef long long ll;
+typedef long double ld;
+
+void solve(){
+  
+}
+
+
+int main() {
+    FIO
+    int m;
+cin >> m;
+while(true) {
+if(m==1)break;
+    if(m%2==0)cout<<(m/2)<<el;
+
+    else cout<< (m*3)+1<<el;
+    
+ 
+    
+    return 0;
+}
+}
