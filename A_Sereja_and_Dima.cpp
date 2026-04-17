467fd2382dbda5c9018d3c859291a4d077026c9a
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-04-17 05:33:48 +0200

    Update A_SEREJA_and_Dima.cpp

--- A_Sereja_and_Dima.cpp
+++ A_Sereja_and_Dima.cpp
@@ -16,7 +16,38 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
-    
-    
+    int n;
+    cin>>n;
+ vector<int> a(n);
+ for(int i=0; i<n; i++){
+ cin>>a[i];}
+
+int l=0, r=n-1;
+int s=0,d=0;
+bool turn=true;// for s
+while(l<=r){
+int pick;
+if (a[r]<a[l]){
+pick=a[l];
+
+l++;}
+
+else {
+
+    pick=a[r];
+  r--;
+}
+if(turn){
+    s+=pick;
+
+}
+else{d+=pick;}
+  turn=!turn;
+}
+
+cout<<s<<" "<<d;
+
+
+
     return 0;
 }
