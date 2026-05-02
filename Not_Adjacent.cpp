033cdff0e9a8b233d69fe8fa9e99b32c6e73c731
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-05-02 16:17:02 +0300

    ADD C - Not Adjacent Problem

--- Not_Adjacent.cpp
+++ Not_Adjacent.cpp
@@ -26,9 +26,29 @@ const ll MOD = 1e9 + 7;
 void SOLVE()
 {
     // بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ
-    
+    string s;
+cin >> s;
+
+const int MOD = 998244353;
+
+long long ans = 0;
+long long len = 1;
+
+ans = 1;
+
+for (int i = 1; i < s.size(); i++) {
+    if (s[i] != s[i - 1]) {
+        len++;
+    } else {
+        len = 1;
+    }
+
+    ans += len;
+    ans %= MOD;
 }
 
+cout << ans;
+}
 int main()
 {
     ios::sync_with_stdio(false);
