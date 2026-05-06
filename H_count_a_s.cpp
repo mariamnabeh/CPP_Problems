4eac243b84744c84620bae0ba4b83cc13b6c81bd
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-05-06 18:24:18 +0300

    ADD PREFIX PROBLEM

---
+++ H_count_a_s.cpp
@@ -0,0 +1,61 @@
+/*
+ ███╗   ███╗ █████╗ ██████╗ ██╗ █████╗ ███╗   ███╗    ███╗   ██╗ █████╗ ██████╗ ███████╗██╗  ██╗
+ ████╗ ████║██╔══██╗██╔══██╗██║██╔══██╗████╗ ████║    ████╗  ██║██╔══██╗██╔══██╗██╔════╝██║  ██║
+ ██╔████╔██║███████║██████╔╝██║███████║██╔████╔██║    ██╔██╗ ██║███████║██████╔╝█████╗  ███████║
+ ██║╚██╔╝██║██╔══██║██╔══██╗██║██╔══██║██║╚██╔╝██║    ██║╚██╗██║██╔══██║██╔══██╗██╔══╝  ██╔══██║
+ ██║ ╚═╝ ██║██║  ██║██║  ██║██║██║  ██║██║ ╚═╝ ██║    ██║ ╚████║██║  ██║██████╔╝███████╗██║  ██║
+ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝     ╚═╝    ╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <numeric>
+
+using namespace std;
+using ll = long long;
+
+#define el '\n'
+const ll MOD = 1e9 + 7;
+
+    // بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ
+void SOLVE()
+{
+    string s;
+    cin >> s;
+    int q;
+    cin>>q;
+
+    int n = s.size();
+
+    vector<ll> prefix(n + 1, 0);
+
+    for (int i = 1; i <= n; i++) {
+        prefix[i] = prefix[i - 1] + (s[i - 1] == 'a');
+    }
+while ((q--))
+{
+ll l, r;
+    cin >> l >> r;
+
+    cout << prefix[r] - prefix[l - 1] << el;
+}
+
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+    
+        SOLVE();
+    
+}
---
+++ templates/prefix.cpp
@@ -0,0 +1,71 @@
+/*
+ ███╗   ███╗ █████╗ ██████╗ ██╗ █████╗ ███╗   ███╗    ███╗   ██╗ █████╗ ██████╗ ███████╗██╗  ██╗
+ ████╗ ████║██╔══██╗██╔══██╗██║██╔══██╗████╗ ████║    ████╗  ██║██╔══██╗██╔══██╗██╔════╝██║  ██║
+ ██╔████╔██║███████║██████╔╝██║███████║██╔████╔██║    ██╔██╗ ██║███████║██████╔╝█████╗  ███████║
+ ██║╚██╔╝██║██╔══██║██╔══██╗██║██╔══██║██║╚██╔╝██║    ██║╚██╗██║██╔══██║██╔══██╗██╔══╝  ██╔══██║
+ ██║ ╚═╝ ██║██║  ██║██║  ██║██║██║  ██║██║ ╚═╝ ██║    ██║ ╚████║██║  ██║██████╔╝███████╗██║  ██║
+ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝     ╚═╝    ╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <numeric>
+
+using namespace std;
+using ll = long long;
+
+#define el '\n'
+const ll MOD = 1e9 + 7;
+
+void SOLVE()
+{
+    // بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ
+
+    string s;
+    cin >> s;
+
+    int n = s.size();
+
+    // prefix array (1-based)
+    vector<int> prefix(n + 1, 0);
+
+    // build prefix
+    for (int i = 1; i <= n; i++) {
+        prefix[i] = prefix[i - 1] + (s[i - 1] == 'a'); 
+        // ممكن تغير الشرط حسب المطلوب
+    }
+
+    int q;
+    cin >> q;
+
+    while (q--) {
+        int l, r;
+        cin >> l >> r;
+
+        cout << prefix[r] - prefix[l - 1] << el;
+    }
+}
+
+
+
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        SOLVE();
+    }
+}
