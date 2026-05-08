13e3aa5da6d4795810880dbc3d63554440e28ceb
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-05-08 23:05:33 +0300

    add divisors template

---
+++ templates/divisors.cpp
@@ -0,0 +1,66 @@
+/*
+ ███╗   ███╗ █████╗ ██████╗ ██╗ █████╗ ███╗   ███╗    ███╗   ██╗ █████╗ ██████╗ ███████╗██╗  ██╗
+ ████╗ ████║██╔══██╗██╔══██╗██║██╔══██╗████╗ ████║    ████╗  ██║██╔══██╗██╔══██╗██╔════╝██║  ██║
+ ██╔████╔██║███████║██████╔╝██║███████║██╔████╔██║    ██╔██╗ ██║███████║██████╔╝█████╗  ███████║
+ ██║╚██╔╝██║██╔══██║██╔══██╗██║██╔══██║██║╚██╔╝██║    ██║╚██╗██║██╔══██║██╔══██╗██╔══╝  ██╔══██║
+ ██║ ╚═╝ ██║██║  ██║██║  ██║██║██║  ██║██║ ╚═╝ ██║    ██║ ╚████║██║  ██║██████╔╝███████╗██║  ██║
+ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝     ╚═╝    ╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <numeric>
+
+using namespace std;
+using ll = long long;
+
+#define el '\n'
+const ll MOD = 1e9 + 7;
+
+void SOLVE()
+{
+    ll n;
+    cin>>n;
+    // بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ
+    vector<long long> divisors;
+    
+    for (long long i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            divisors.push_back(i); // القاسم الأول
+            
+       
+            if (i * i != n) {
+                divisors.push_back(n / i);
+            }
+        }
+    }
+    
+    sort(divisors.begin(), divisors.end());
+for(auto x:divisors){
+
+    cout<<n<<el;
+}
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        SOLVE();
+    }
+}
+
+
+
