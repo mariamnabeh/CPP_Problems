48423dd368a9dda837b152b8da03542d31834ba7
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-05-13 14:38:22 +0300

    add one problem to math challenge

---
+++ A_Elephant.cpp
@@ -0,0 +1,50 @@
+/*
+ ███╗   ███╗ █████╗ ██████╗ ██╗ █████╗ ███╗   ███╗    ███╗   ██╗ █████╗ ██████╗ ███████╗██╗  ██╗
+ ████╗ ████║██╔══██╗██╔══██╗██║██╔══██╗████╗ ████║    ████╗  ██║██╔══██╗██╔══██╗██╔════╝██║  ██║
+ ██╔████╔██║███████║██████╔╝██║███████║██╔████╔██║    ██╔██╗ ██║███████║██████╔╝█████╗  ███████║
+ ██║╚██╔╝██║██╔══██║██╔══██╗██║██╔══██║██║╚██╔╝██║    ██║╚██╗██║██╔══██║██╔══██╗██╔══╝  ██╔══██║
+ ██║ ╚═╝ ██║██║  ██║██║  ██║██║██║  ██║██║ ╚═╝ ██║    ██║ ╚████║██║  ██║██████╔╝███████╗██║  ██║
+ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝     ╚═╝    ╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <numeric>
+
+using namespace std;
+using ll = long long;
+
+#define el '\n'
+const ll MOD = 1e9 + 7;
+
+
+    // بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ
+void SOLVE(){
+// day 2 of challenge : every day atleast one math problem :)
+    // draft: we have n num of steps from 0 : 5; if we / of num (smaller than 5 like 4) we back two know how many steps
+    // why 4 , if we try 3 and n = 1 , the answer will be less then 1 , and we will not know the num of steps
+ll n;
+cin>>n;
+n=(n+4)/5;
+cout<<n;
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        SOLVE();
+    }
+}
