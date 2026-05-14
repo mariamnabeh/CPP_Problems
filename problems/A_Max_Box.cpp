980ed2d9e9367574a87275f95ac3a1a496dec035
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-05-14 20:50:47 +0300

    add some problems to problems folder

---
+++ problems/A_Max_Box.cpp
@@ -0,0 +1,63 @@
+/*
+ ███╗   ███╗ █████╗ ██████╗ ██╗ █████╗ ███╗   ███╗    ███╗   ██╗ █████╗ ██████╗ ███████╗██╗  ██╗
+ ████╗ ████║██╔══██╗██╔══██╗██║██╔══██╗████╗ ████║    ████╗  ██║██╔══██╗██╔══██╗██╔════╝██║  ██║
+ ██╔████╔██║███████║██████╔╝██║███████║██╔████╔██║    ██╔██╗ ██║███████║██████╔╝█████╗  ███████║
+ ██║╚██╔╝██║██╔══██║██╔══██╗██║██╔══██║██║╚██╔╝██║    ██║╚██╗██║██╔══██║██╔══██╗██╔══╝  ██╔══██║
+ ██║ ╚═╝ ██║██║  ██║██║  ██║██║██║  ██║██║ ╚═╝ ██║    ██║ ╚████║██║  ██║██████╔╝███████╗██║  ██║
+ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝     ╚═╝    ╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <numeric>
+
+using namespace std;
+using ll = long long;
+
+#define el '\n'
+const ll MOD = 1e9 + 7;
+
+void SOLVE()
+{
+    // بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ
+   ll n, k;
+   cin>>n>>k;
+   ll mx=0;
+   ll sum=0;
+   vector<ll>input(n+1);
+   vector<ll>prefix(n+1, 0);
+   for (int i =1; i<=n; i++){
+
+    cin>>input[i];
+   }
+
+for(int i=1; i<=n; i++){
+prefix[i]=prefix[i-1]+input[i];}
+for(int i=k; i<=n;i++){
+sum=prefix[i]-prefix[i-k];
+mx=max(sum, mx);
+}
+
+
+cout<<mx;
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        SOLVE();
+    }
+}
