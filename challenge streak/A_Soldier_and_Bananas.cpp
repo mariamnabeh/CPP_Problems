0597e1da901bde935df32665696e20cae7f3fca6
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-05-15 12:09:35 +0300

    ADD 2 PROBLEMS TO MATH CHALLENGE

--- challenge streak/A_Soldier_and_Bananas.cpp
+++ challenge streak/A_Soldier_and_Bananas.cpp
@@ -20,7 +20,7 @@
 using namespace std;
 using ll = long long;
 
-#define el '\n
+#define el '\n'
 const ll MOD = 1e9 + 7;
 
 void SOLVE()
@@ -28,11 +28,22 @@ void SOLVE()
     // بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ
     // day 3 of challenge : every day atleast one math problem :)
 
+ll k,n,w;
+cin>>k>>n>>w;
+ll amount=0;
+ll current=0;
+for(int i=1; i<=w; i++){
+current+=k*i;
 
+}
 
+amount=current-n;
+if (amount>0){
+cout<<amount;}
+else{
+cout<<0;
 
-
-
+}
 
 
 }
