eed917744fec5f7ad0bf097ad0bc6ea6fc4ea91a
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-05-15 19:08:12 +0300

    add one problem to problem

---
+++ problems/D_Odd_Queries.cpp
@@ -0,0 +1,69 @@
+/*
+ ███╗   ███╗ █████╗ ██████╗ ██╗ █████╗ ███╗   ███╗    ███╗   ██╗ █████╗ ██████╗ ███████╗██╗  ██╗
+ ████╗ ████║██╔══██╗██╔══██╗██║██╔══██╗████╗ ████║    ████╗  ██║██╔══██╗██╔══██╗██╔════╝██║  ██║
+ ██╔████╔██║███████║██████╔╝██║███████║██╔████╔██║    ██╔██╗ ██║███████║██████╔╝█████╗  ███████║
+ ██║╚██╔╝██║██╔══██║██╔══██╗██║██╔══██║██║╚██╔╝██║    ██║╚██╗██║██╔══██║██╔══██╗██╔══╝  ██╔══██║
+ ██║ ╚═╝ ██║██║  ██║██║  ██║██║██║  ██║██║ ╚═╝ ██║    ██║ ╚████║██║  ██║██████╔╝███████╗██║  ██║
+ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝     ╚═╝    ╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <numeric>
+
+using namespace std;
+using ll = long long;
+
+#define el '\n'
+const ll MOD = 1e9 + 7;
+
+void SOLVE()
+{
+    // بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ
+ll n, q;
+cin>>n>>q;
+vector<ll>a(n+1);
+vector<ll> prefix(n+1);
+for(ll i=1; i<=n; i++){
+cin>> a[i];
+prefix[i] = prefix[i - 1] + a[i];
+
+}
+ll l, r,k;
+while(q--){
+cin>>l>>r>>k;
+ll sum=prefix[l-1]-prefix[r];
+ll new_sum=prefix[n] -sum +(r-l+1)*k;
+
+if (new_sum%2!=0){
+
+cout<<"YES"<<el;
+
+}
+ else {cout<<"NO" <<el;  }
+}
+
+
+}
+
+
+
+
+ main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t ;
+     cin >> t;
+    while (t--)
+    {
+        SOLVE();
+    }
+}
