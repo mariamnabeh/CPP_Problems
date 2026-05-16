7f8e8fc694bf649dab6996f1d757a0e681a69dad
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-05-17 02:50:59 +0300

    Add problem for day 5 , start solving problem day six!

---
+++ challenge streak/B_Relatively_Prime_Pairs.cpp
@@ -0,0 +1,56 @@
+/*
+ ███╗   ███╗ █████╗ ██████╗ ██╗ █████╗ ███╗   ███╗    ███╗   ██╗ █████╗ ██████╗ ███████╗██╗  ██╗
+ ████╗ ████║██╔══██╗██╔══██╗██║██╔══██╗████╗ ████║    ████╗  ██║██╔══██╗██╔══██╗██╔════╝██║  ██║
+ ██╔████╔██║███████║██████╔╝██║███████║██╔████╔██║    ██╔██╗ ██║███████║██████╔╝█████╗  ███████║
+ ██║╚██╔╝██║██╔══██║██╔══██╗██║██╔══██║██║╚██╔╝██║    ██║╚██╗██║██╔══██║██╔══██╗██╔══╝  ██╔══██║
+ ██║ ╚═╝ ██║██║  ██║██║  ██║██║██║  ██║██║ ╚═╝ ██║    ██║ ╚████║██║  ██║██████╔╝███████╗██║  ██║
+ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝     ╚═╝    ╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <numeric>
+using namespace std;
+using ll = long long;
+
+#define el '\n' 
+const ll MOD = 1e9 + 7;
+
+void SOLVE()
+{
+    // بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ
+     // day 5 of challenge : every day atleast one math problem :)
+
+// Since (r - l + 1) is even, we can pair all numbers in the range.
+// We take consecutive pairs (x, x+1) because gcd(x, x+1) = 1 always.
+// So this guarantees a valid solution for all elements.
+
+
+ll l , r;
+cin>>l>>r;
+cout<<"YES"<<endl;
+for(ll i=l; i<r; i+=2){
+cout<< i<<" "<< i+1<<endl;
+
+
+}
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+   ll t = 1;
+     //cin >> t;
+    while (t--)
+    {
+        SOLVE();
+    }
+}
