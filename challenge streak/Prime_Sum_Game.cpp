d20bab9ae4bb46496db9d230e7eb50e893725441
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-05-27 17:26:18 +0300

    Add one problem to math streak

---
+++ challenge streak/Prime_Sum_Game.cpp
@@ -0,0 +1,84 @@
+/*
+ ███╗   ███╗ █████╗ ██████╗ ██╗ █████╗ ███╗   ███╗    ███╗   ██╗ █████╗ ██████╗ ███████╗██╗  ██╗
+ ████╗ ████║██╔══██╗██╔══██╗██║██╔══██╗████╗ ████║    ████╗  ██║██╔══██╗██╔══██╗██╔════╝██║  ██║
+ ██╔████╔██║███████║██████╔╝██║███████║██╔████╔██║    ██╔██╗ ██║███████║██████╔╝█████╗  ███████║
+ ██║╚██╔╝██║██╔══██║██╔══██╗██║██╔══██║██║╚██╔╝██║    ██║╚██╗██║██╔══██║██╔══██╗██╔══╝  ██╔══██║
+ ██║ ╚═╝ ██║██║  ██║██║  ██║██║██║  ██║██║ ╚═╝ ██║    ██║ ╚████║██║  ██║██████╔╝███████╗██║  ██║
+ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝     ╚═╝    ╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+
+using namespace std;
+using ll = long long;
+
+#define el '\n' 
+const ll MOD = 1e9 + 7;
+
+void SOLVE()
+{
+    // بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ
+    // day 9  of challenge : every day atleast one math problem :)
+
+ll a,b,c,d;
+cin>>a>>b>>c>>d;
+for(int x=a ; x<=b; x++){
+
+bool ok=false;
+for(int y=c; y<=d; y++){
+int sum = x + y;
+bool prime=true;
+if(sum<2){
+    prime=false;
+}
+
+for(int i=2; i*i<=sum ; i++){
+
+if(sum%i==0){
+prime=false;
+break;
+
+
+}}
+if(prime){
+ok = true;
+  break;}
+
+
+
+}
+
+if(!ok){
+
+cout<<"Takahashi";
+
+return;
+}}
+
+cout << "Aoki";
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        SOLVE();
+    }
+    return 0;
+}
