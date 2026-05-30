fbce2613769d0ff610c8f47ff71eb7ab375659ad
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-05-30 21:02:19 +0300

    Add problem to problems folder

--- problems/B_Two_gram.cpp
+++ problems/B_Two_gram.cpp
@@ -28,25 +28,39 @@ const ll MOD = 1e9 + 7;
 void SOLVE()
 {
     // بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ
-    
+    ll n ; string s;
+    cin>>n>>s;
+    string ans="";
+ll best=0;
+map<string, int> freq;
+for(int i=0; i<n-1; i++){
+string t = "";
+t += s[i];
+t += s[i+1];
+freq[t]++;
+if(best<freq[t]){
+best=freq[t];
+ans =t;
 
-
-
-
-
-
-
-
-    
 }
+
+}
+
+
+cout<<ans<<el;
+
+
+
+    }
+
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    ll t ;
-     cin >> t;
+    ll t = 1;
+    // cin >> t;
     while (t--)
     {
         SOLVE();
