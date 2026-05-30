f0c1e0d6e82413f76dd49ce397ec532336f8e1b1
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-05-30 16:35:41 +0300

    Remove some tempcode files

--- H_Maximal_AND.cpp
+++ H_Maximal_AND.cpp
@@ -24,23 +24,20 @@ using ll = long long;
 
 #define el '\n' 
 const ll MOD = 1e9 + 7;
- bool checkbit(int n ,int i){
-return (n>>i)&1;
 
-
- }
 void SOLVE()
 {
     // بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ
-    ll n , k;cin>>n>>k;
-    ll ans=0;
-    for(int i=0; i<k; i++){
+    
 
-if (checkbit(n, i)){ans++;}
-}
 
-cout<<ans<<el;
 
+
+
+
+
+
+    
 }
 
 int main()
@@ -48,8 +45,8 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    ll t = 1;
-    // cin >> t;
+    ll t ;
+     cin >> t;
     while (t--)
     {
         SOLVE();
