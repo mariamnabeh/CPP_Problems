5ec312576b316b815feac061091cde7de908eeb6
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-05-30 16:32:08 +0300

    Add problem to challenge streak folder

---
+++ challenge streak/Number_Modulopower of_2.cpp
@@ -0,0 +1,70 @@
+/*
+ ███╗   ███╗ █████╗ ██████╗ ██╗ █████╗ ███╗   ███╗    ███╗   ██╗ █████╗ ██████╗ ███████╗██╗  ██╗
+ ████╗ ████║██╔══██╗██╔══██╗██║██╔══██╗████╗ ████║    ████╗  ██║██╔══██╗██╔══██╗██╔════╝██║  ██║
+ ██╔████╔██║███████║██████╔╝██║███████║██╔████╔██║    ██╔██╗ ██║███████║██████╔╝█████╗  ███████║
+ ██║╚██╔╝██║██╔══██║██╔══██╗██║██╔══██║██║╚██╔╝██║    ██║╚██╗██║██╔══██║██╔══██╗██╔══╝  ██╔══██║
+ ██║ ╚═╝ ██║██║  ██║██║  ██║██║██║  ██║██║ ╚═╝ ██║    ██║ ╚████║██║  ██║██████╔╝███████╗██║  ██║
+ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝     ╚═╝    ╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+
+using namespace std;
+using ll = long long;
+#define el '\n' 
+const ll MOD = 1e9 + 7;
+ // day 10  of challenge : every day atleast one math problem :)
+
+// the idea :
+// n is not divisible by 2^k because some of the lowest k bits are ON.
+// Every ON bit must be removed.
+// One operation can remove one such bit by subtracting its power of 2.
+// So the answer is the number of ON bits among the first k bits.
+
+
+
+
+
+
+
+ bool checkbit(int n ,int i){
+return (n>>i)&1;
+
+
+ }
+void SOLVE()
+{
+    // بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ
+    ll n , k;cin>>n>>k;
+    ll ans=0;
+    for(int i=0; i<k; i++){
+
+if (checkbit(n, i)){ans++;}
+}
+
+cout<<ans<<el;
+
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        SOLVE();
+    }
+    return 0;
+}
