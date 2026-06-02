64aaf02bc1c870ab6945656b1e3ed4421c438c14
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-06-02 19:26:44 +0300

    Added prefix_xor.cpp to templetes folder

---
+++ templates/prefix_xor.cpp
@@ -0,0 +1,79 @@
+/*
+ ███╗   ███╗ █████╗ ██████╗ ██╗ █████╗ ███╗   ███╗    ███╗   ██╗ █████╗ ██████╗ ███████╗██╗  ██╗
+ ████╗ ████║██╔══██╗██╔══██╗██║██╔══██╗████╗ ████║    ████╗  ██║██╔══██╗██╔══██╗██╔════╝██║  ██║
+ ██╔████╔██║███████║██████╔╝██║███████║██╔████╔██║    ██╔██╗ ██║███████║██████╔╝█████╗  ███████║
+ ██║╚██╔╝██║██╔══██║██╔══██╗██║██╔══██║██║╚██╔╝██║    ██║╚██╗██║██╔══██║██╔══██╗██╔══╝  ██╔══██║
+ ██║ ╚═╝ ██║██║  ██║██║  ██║██║██║  ██║██║ ╚═╝ ██║    ██║ ╚████║██║  ██║██████╔╝███████╗██║  ██║
+ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝     ╚═╝    ╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+
+using namespace std;
+using ll = long long;
+
+#define el '\n' 
+const ll MOD = 1e9 + 7;
+
+void SOLVE()
+{
+    // بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ
+ ll n;
+ cin>>n;
+ vector <int> a(n+1), prefix(n+1);
+ for(int i=1; i<=n; i++){
+
+    cin>>a[i];}
+    
+for(int i=1; i<=n; i++){
+prefix[i]=prefix[i-1]^ a[i];
+
+}
+
+ll q; cin>>q;
+while(q--){
+ll r, l;
+cin>>r>>l;
+prefix[r]^prefix[l-1];
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        SOLVE();
+    }
+    return 0;
+}
