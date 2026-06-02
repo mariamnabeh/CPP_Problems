b51870820c75b51bb0c0181706da02bac152a01f
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-06-02 22:52:56 +0300

    add problem to problems folder

---
+++ prefix_and.cpp
@@ -0,0 +1,78 @@
+/*
+ ███╗   ███╗ █████╗ ██████╗ ██╗ █████╗ ███╗   ███╗    ███╗   ██╗ █████╗ ██████╗ ███████╗██╗  ██╗
+ ████╗ ████║██╔══██╗██╔══██╗██║██╔══██╗████╗ ████║    ████╗  ██║██╔══██╗██╔══██╗██╔════╝██║  ██║
+ ██╔████╔██║███████║██████╔╝██║███████║██╔████╔██║    ██╔██╗ ██║███████║██████╔╝█████╗  ███████║
+ ██║╚██╔╝██║██╔══██║██╔══██╗██║██╔══██║██║╚██╔╝██║    ██║╚██╗██║██╔══██║██╔══██╗██╔══╝  ██╔══██║
+ ██║ ╚═╝ ██║██║  ██║██║  ██║██║██║  ██║██║ ╚═╝ ██║    ██║ ╚████║██║  ██║██████╔╝███████╗██║  ██║
+ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝     ╚═╝    ╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+
+using namespace std;
+using ll = long long;
+
+#define el '\n' 
+const ll MOD = 1e9 + 7;
+
+void SOLVE()
+{
+    // بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ
+    ll n;
+    cin>>n;
+    vector<ll>a(n+1);
+    for(int i=1; i<=n; i++){
+cin>>a[i];
+    }
+vector<vector<int>>prefix(32,vector<int>(n));
+for (ll bit = 0; bit< 32; bit++)
+{
+    for (ll j= 0; j<=bit; j++)
+    {
+    prefix[bit][j]=prefix[bit][j-1]+(a[j]>>bit&1);
+    }
+    
+}
+
+
+int ans=0;
+ll q;
+cin>>q;
+while(q--){
+ll r, l;
+cin>>r>>l;
+for (ll bit = 0; bit< 32; bit++){
+
+    int count=prefix[bit][r]-prefix[bit][l-1];
+    if (count =r-l+1) ans+=bit<<1;
+   
+}
+}
+
+cout<<ans<<el;
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        SOLVE();
+    }
+    return 0;
+}
