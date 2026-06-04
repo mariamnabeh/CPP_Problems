a66dd452a6f35ca4a73c7067fc52d43551e918a8
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-06-04 17:44:33 +0300

    Create A_Dawid_and_Bags_of_Candies.cpp

---
+++ problems/A_Dawid_and_Bags_of_Candies.cpp
@@ -0,0 +1,80 @@
+/*
+ ███╗   ███╗ █████╗ ██████╗ ██╗ █████╗ ███╗   ███╗    ███╗   ██╗ █████╗ ██████╗ ███████╗██╗  ██╗
+ ████╗ ████║██╔══██╗██╔══██╗██║██╔══██╗████╗ ████║    ████╗  ██║██╔══██╗██╔══██╗██╔════╝██║  ██║
+ ██╔████╔██║███████║██████╔╝██║███████║██╔████╔██║    ██╔██╗ ██║███████║██████╔╝█████╗  ███████║
+ ██║╚██╔╝██║██╔══██║██╔══██╗██║██╔══██║██║╚██╔╝██║    ██║╚██╗██║██╔══██║██╔══██╗██╔══╝  ██╔══██║
+ ██║ ╚═╝ ██║██║  ██║██║  ██║██║██║  ██║██║ ╚═╝ ██║    ██║ ╚████║██║  ██║██████╔╝███████╗██║  ██║
+ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝     ╚═╝    ╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+
+using namespace std;
+using ll = long long;
+
+#define el '\n' 
+const ll MOD = 1e9 + 7;
+
+void SOLVE()
+{
+    // بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ
+int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    int total = a + b + c + d;
+
+    if (total % 2 != 0)
+    {
+        cout << "NO";
+        return;
+    }
+
+    int target = total / 2;
+
+  
+    int arr[4] = {a, b, c, d};
+
+    for (int mask = 0; mask < (1 << 4); mask++)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (mask & (1 << i))
+                sum += arr[i];
+        }
+
+        if (sum == target)
+        {
+            cout << "YES";
+            return;
+        }
+    }
+
+    cout << "NO";
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        SOLVE();
+    }
+    return 0;
+}
